4f7b42c591bf208e316f9ed4642851f28277399b
tamirazrab <tamirazrab@gmail.com>
2020-07-03 21:36:03 +0500

    Added input field

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hi there!');
+const fetchData = async (itemSearch) => {
+	const response = await axios.get("http://www.omdbapi.com/", {
+		params: {
+			apikey: "f2cb1c88",
+			s: itemSearch,
+		},
+	});
+
+	console.log(response.data);
+};
+
+const search = document.getElementById("movie-search");
+search.addEventListener("input", function (event) {
+	fetchData(event.target.value); // Data user types into input box
+	// Not ideal as search request being sent every time user hits single
+	// key stroke
+});
