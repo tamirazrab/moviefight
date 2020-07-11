395704136dc9497ebdc5fe1a15a6b190ce3a583e
tamirazrab <tamirazrab@gmail.com>
2020-07-11 11:22:37 +0500

    Added movie template - displaying information

--- index.js
+++ index.js
@@ -79,5 +79,46 @@ const onMovieSelect = async (movie) => {
 		},
 	});
 
-	console.log(response.data);
+	document.querySelector('.outputTest').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetails /* contains all details */) => {
+	return `
+	<article class="media">
+		<figure class="media-left">
+				<img src="${movieDetails.Poster}" />
+ 			</p >
+	</figure >
+	<div class="media-content">
+		<div class="content">
+			<h1>${movieDetails.Title}</h1>
+			<h4>${movieDetails.Genre}</h4>
+			<p>${movieDetails.Plot}</p>
+		</div>
+	</div>
+	</article >
+
+	<article class="notification is-primary">
+		<p class="title">${movieDetails.Awards}</p>
+		<p class="subtitle">Awards</p>
+	</article>
+	<article class="notification is-info">
+		<p class="title">${movieDetails.BoxOffice}</p>
+		<p class="subtitle">Box Office</p>
+	</article>
+	<article class="notification is-info">
+		<p class="title">${movieDetails.Metascore}</p>
+		<p class="subtitle">Metascore</p>
+	</article>
+<article class="notification is-info">
+	<p class="title">${movieDetails.imdbRating}</p>
+	<p class="subtitle">IMDB Rating</p>
+</article>
+<article class="notification is-info">
+	<p class="title">${movieDetails.imdbVotes}</p>
+	<p class="subtitle">IMDB Votes</p>
+</article>
+
+
+	`;
 };
